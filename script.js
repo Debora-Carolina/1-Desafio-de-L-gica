ebd792d1243182b93e4a0c99e52da01488872442
Deboracarolina <deboracarolina1@hotmail.com>
2023-12-08 23:47:58 -0300

    1° Desafio de Lógica

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log ("Ano novo chegando")
+
+let cantora =- "Mariah Carey";
+
+let idade = 54;
+
+let ano = 2023;
+
+console.log(ano - idade);
+
+const anoNascimento = ano - idade;
+
+console.log(anoNascimento);
+
+
+
